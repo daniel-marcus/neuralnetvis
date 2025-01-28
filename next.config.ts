c53363ec71d0d254af1233cb0ba3fcbfc46f9e26
Daniel Marcus <dm@danielmarcus.de>
2025-01-28 15:23:35 +0100

    add favicon / version

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next"
+import { version } from "./package.json"
 
 const nextConfig: NextConfig = {
   /* config options here */
   // reactStrictMode: false,
+  env: {
+    APP_VERSION: version,
+  },
 }
 
 export default nextConfig
--- src/components/console.tsx
+++ src/components/console.tsx
@@ -4,7 +4,7 @@ export const Console = () => {
   const [entries] = useState<string[]>([])
   return (
     <div className="fixed top-0 left-0 p-4 text-gray-600">
-      <div>Neuro Vis 0.1.0</div>
+      <div>Neuro Vis {process.env.APP_VERSION}</div>
       {entries.slice(-5).map((entry, i) => (
         <div key={i} dangerouslySetInnerHTML={{ __html: entry }}></div>
       ))}
