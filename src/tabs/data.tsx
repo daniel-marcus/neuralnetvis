5edb8d4f18b06cb14c3605042399c67f08fdc2e7
Daniel Marcus <dm@danielmarcus.de>
2025-02-05 23:51:04 +0100

    minor fixes

--- src/tabs/data.tsx
+++ src/tabs/data.tsx
@@ -1,15 +1,17 @@
 import { Box, MenuBtn, Slider } from "@/ui-components"
 import { datasets, useDatasetStore } from "@/lib/datasets"
+import { useDebugStore } from "@/lib/debug"
 
 export const Data = () => {
   const currDatasetKey = useDatasetStore((s) => s.datasetKey)
   const setDatasetKey = useDatasetStore((s) => s.setDatasetKey)
+  const isDebug = useDebugStore((s) => s.debug)
   return (
     <Box>
       <div className="flex flex-col gap-2">
         <div className="flex flex-col">
           {datasets
-            .filter((d) => !d.disabled)
+            .filter((d) => isDebug || !d.disabled)
             .map((d) => (
               <MenuBtn
                 key={d.name}
