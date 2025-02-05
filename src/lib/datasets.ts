81ddd66a856bb09ed34a09ec6c36ca7ce032466b
Daniel Marcus <dm@danielmarcus.de>
2025-02-06 00:14:30 +0100

    add cifar10 in chunks

--- src/lib/datasets.ts
+++ src/lib/datasets.ts
@@ -143,7 +143,6 @@ export const datasets: DatasetDef[] = [
     name: "cifar10",
     task: "classification",
     description: "Color images (32x32x3)",
-    disabled: true,
     aboutUrl: "https://www.cs.toronto.edu/~kriz/cifar.html",
     loss: "categoricalCrossentropy",
     output: {
@@ -163,17 +162,23 @@ export const datasets: DatasetDef[] = [
       ],
     },
     loadData: async () => {
-      const [xTrain, yTrain, xTest, yTest] = await fetchMutlipleNpzWithProgress(
-        [
-          "/data/cifar10_20k/x_train.npz",
+      const [xTrain1, xTrain2, xTrain3, yTrain, xTest, yTest] =
+        await fetchMutlipleNpzWithProgress([
+          "/data/cifar10_20k/x_train_1.npz",
+          "/data/cifar10_20k/x_train_2.npz",
+          "/data/cifar10_20k/x_train_3.npz",
           "/data/cifar10_20k/y_train.npz",
           "/data/cifar10_20k/x_test.npz",
           "/data/cifar10_20k/y_test.npz",
-        ]
-      )
+        ])
       return tf.tidy(() => {
+        const combinedTrainX = tf.concat([
+          tf.tensor(xTrain1.data, xTrain1.shape),
+          tf.tensor(xTrain2.data, xTrain2.shape),
+          tf.tensor(xTrain3.data, xTrain3.shape),
+        ])
         // normalize: vals / 255
-        const trainX = tf.tensor(xTrain.data, xTrain.shape).div(255)
+        const trainX = combinedTrainX.div(255)
         const testX = tf.tensor(xTest.data, xTest.shape).div(255)
         const trainY = tf.oneHot(yTrain.data, 10)
         const testY = tf.oneHot(yTest.data, 10)
@@ -396,10 +401,8 @@ async function fetchMutlipleNpzWithProgress(paths: string[]) {
     const text = percent < 1 ? "Loading dataset ..." : "Dataset loaded"
     setStatusText(text, { percent })
   }
-  const allPromises = paths.map((path) =>
-    fetchWithProgress(path, onProgress, { cache: "force-cache" }).then((r) =>
-      r.arrayBuffer()
-    )
+  const allPromises = paths.map(
+    (path) => fetchWithProgress(path, onProgress).then((r) => r.arrayBuffer()) // , { cache: "force-cache" }
   )
   const allFiles = await Promise.all(allPromises)
   const allParsed = await Promise.all(allFiles.map(parseNpz))
