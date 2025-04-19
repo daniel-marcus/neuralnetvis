daf11b8fc8df0d2e79c2b0fed55b9d3063885620
Daniel Marcus <dm@danielmarcus.de>
2025-04-19 10:38:35 +0200

    rm blood mnist

--- src/data/datasets/index.ts
+++ src/data/datasets/index.ts
@@ -5,11 +5,9 @@ import { cifar10 } from "./cifar10"
 import { cifar100 } from "./cifar100"
 import { californiaHousing } from "./california-housing"
 import { autoMpg } from "./auto-mpg"
-import { bloodMnist } from "./blood-mnist"
 import type { DatasetDef } from "@/data/types"
 
 export const datasets: DatasetDef[] = [
-  bloodMnist,
   fashionMnist,
   handPose,
   mnist,
