017aec87f602d2cf8864f9d8a38c907b405f04f7
Daniel Marcus <dm@danielmarcus.de>
2025-12-29 14:25:17 +0100

    chore(screenshot): add error handling

--- scripts/screenshot.ts
+++ scripts/screenshot.ts
@@ -2,16 +2,16 @@ import fs from "fs"
 import path from "path"
 import puppeteer from "puppeteer"
 
-const PORT = 3000
+const PAGEROOT = `http://localhost:3000`
 
 const WIDTH = 1200
 const HEIGHT = 630
 
 const DOCROOT = "out/"
 const SCREENSHOT_FOLDER = "public/images/og"
-const paths = ["", ...getPaths()].filter((p) => !["index", "404"].includes(p))
-
-console.log(paths)
+const paths = ["", ...getPaths()].filter(
+  (p) => !["index", "404", "_not-found"].includes(p),
+)
 
 async function run() {
   fs.rmSync(SCREENSHOT_FOLDER, { recursive: true, force: true })
@@ -24,11 +24,21 @@ async function run() {
   })
   const page = await browser.newPage()
 
+  try {
+    await page.goto(PAGEROOT)
+  } catch {
+    const bold = (str: string) => `\x1b[1m${str}\x1b[0m`
+    console.error(
+      `Error: Could not connect to ${PAGEROOT}. \nRun ${bold("pnpm build && pnpm serve")} before executing this script.\n`,
+    )
+    process.exit(1)
+  }
+
   for (const p of paths) {
-    await page.goto(`http://localhost:${PORT}/${p}?screenshot`)
+    await page.goto(`${PAGEROOT}/${p}?screenshot`)
     const title = await page.title()
     console.log(title)
-    await new Promise((resolve) => setTimeout(resolve, 1500))
+    await new Promise((resolve) => setTimeout(resolve, 2000))
     const path = `${SCREENSHOT_FOLDER}/${
       pathToFileName(p) || "default"
     }.png` as `${string}.png`
