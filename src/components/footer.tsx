239b41771ccf74b66ac0d341eeb870fa7ce34a1c
Daniel Marcus <dm@danielmarcus.de>
2026-02-01 20:02:54 +0100

    build(deps): bump @mediapipe/tasks-vision to latest version

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ export const Footer = ({ className = "" }) => {
   return (
     <div className={`py-16 text-right ${className}`}>
       <p>
-        © 2025 by{" "}
+        © 2026 by{" "}
         <ExtLink href="https://danielmarcus.de/">Daniel Marcus</ExtLink> |{" "}
         <ExtLink href="https://github.com/daniel-marcus/neuralnetvis">
           Github
